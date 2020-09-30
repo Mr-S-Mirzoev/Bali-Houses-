0bd416d5dcb34c0b592ad968d497f53e7eae56d3
Kozhukh Polina <kozhukhpolinaa@gmail.com>
2020-09-30 15:15:33 +0300

    Got rid of legacy

--- Legacy/resolver.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import os
-
-def resolve_dep(package):
-    bashCommand = "pip3 install " + package
-    process = subprocess.Popen(bashCommand.split(), stdout=open(os.devnull, 'wb'))
-    process.wait()
